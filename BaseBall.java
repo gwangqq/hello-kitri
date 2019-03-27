e74b40e33ec8ebb0319436a5587fbb4f4e889b73
gwangq <pgg961@gmail.com>
2019-03-27 20:57:38 +0900

    2019 03 27
    BaseBall 예제 일단 만든것
    완벽하지 않지만 일단 한 곳 까지

--- BaseBall.java
+++ BaseBall.java
@@ -1,60 +1,68 @@
-package com.kitri.awt.design;
+package javaexercise;
 
 import java.awt.*;
 
-import com.sun.javafx.collections.SetListenerHelper;
-
-
-public class BaseBall extends Frame{
-//		선언부
+public class BaseBall extends Frame {
+//	왼쪽 panel 스크롤바/ 오른쪽 panel colorP
+//	왼쪽 panel 
+	Panel pW = new Panel();
 	
-
-	Panel pR = new Panel();
-	Panel pS = new Panel();
+	Panel pV = new Panel(); // 비어있는 스크롤바
+	String v = "  ";
+	Panel pR = new Panel(); // 빨간 스크롤바
+	Scrollbar sbR = new Scrollbar(Scrollbar.HORIZONTAL, 100, 10, 0, 255)	;
+	Label lR = new Label("빨강");
 	
-
-//	pR에 들어가야할 버튼 5개
-	Button newG = new Button("새게임");
-	Button clear = new Button("지우기");
-	Button dop = new Button("정 답");
-	Button fontC = new Button("글자색");
-	Button exit = new Button("종 료");
-
-//	pC에 들어갈 ta
+	Panel pG = new Panel(); // 초록 스크롤바
+	Scrollbar sbG = new Scrollbar(Scrollbar.HORIZONTAL, 1000, 10, 0, 255)	;
+	Label lG = new Label("초록");
 	
-	TextArea ta = new TextArea();
+	Panel pB = new Panel(); // 파란 스크롤바
+	Scrollbar sbB = new Scrollbar(Scrollbar.HORIZONTAL, 1000, 10, 0, 255)	;
+	Label lB = new Label("파랑");
 	
 	
-//	pS에 들어갈 숫자 l, tf
 	
-	Label l = new Label("숫자");
+//	오른쪽 panel  
+	Panel d = new Panel();
+	Panel colorP = new Panel();
+	Button ok = new Button("ok");
 	TextField tf = new TextField();
+	TextArea tA = new TextArea();
 	
-
-	
-	public BaseBall() {
-//		각 패널 디자인 구현
-		super ("BaseBall");
-//		pR 디자인 GridLayout으로 구현
-		pR.setLayout(new GridLayout(5,1,0,10));
-		pR.add(newG);
-		pR.add(clear);
-		pR.add(dop);
-		pR.add(fontC);
-		pR.add(exit);
- 		
-		pS.setLayout(new BorderLayout());
-		pS.add(l, "West");
-		pS.add(tf, "Center");
+	public BaseBall( ) {
+		super("BaseBall!!!!");
 		
 		
-		setLayout(new BorderLayout());
-		add(pR, "East");
-		add(pS, "South");
+//		오른쪽 panel 작업
+		
+		d.setLayout(new GridLayout(1,1));
+		d.add(tf,"West");
+		d.add(ok,"East");
+		
+		colorP.setLayout(new BorderLayout());
+		colorP.add(tA, "Center");
+		colorP.add(d, "South");
+		
+//		왼쪽 panel 작업
+		
+		pW.setLayout(new GridLayout(3,2, 10, 50));
+		
+		pW.add(lR);
+		pW.add(sbR);
+		pW.add(lG);
+		pW.add(sbG);
+		pW.add(lB);
+		pW.add(sbB);		
+		
+		setLayout(new GridLayout(1,1));
+		add(pW);
+		add(colorP);
 		setBounds(300, 200, 300, 400);
 		setVisible(true);
+		
+		
 	}
-	
 	public static void main(String[] args) {
 		new BaseBall();
 	}
